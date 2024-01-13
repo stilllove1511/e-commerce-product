ffb2b05e0a60f2636ab8f887763185ebd7bbca22
milkyway <milkyway1511@outlook.com>
2024-01-13 14:06:32 +0700

    Add Eureka integration and update dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { ConfigModule } from '@nestjs/config'
 import { ProductModule } from './modules/product/product.module'
 import { BrandModule } from './modules/brand/brand.module'
 import { CategoryModule } from './modules/category/category.module'
+import { EurekaModule } from 'nestjs-eureka'
 
 @Module({
     imports: [
@@ -12,6 +13,20 @@ import { CategoryModule } from './modules/category/category.module'
         ProductModule,
         BrandModule,
         CategoryModule,
+        EurekaModule.forRoot({
+            eureka: {
+              host: process.env.EUREKA_HOST,
+              port: process.env.EUREKA_PORT,    
+              registryFetchInterval: 1000,
+              servicePath: '/eureka/apps/',
+              maxRetries: 3,
+            },
+            service: {
+              name: 'my-service',
+              port: 8080,
+            },
+        }),
+        
     ],
 })
 export class AppModule {}
